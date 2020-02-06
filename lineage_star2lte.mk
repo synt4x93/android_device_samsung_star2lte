0813cad4b3cb52dbe9d93041544a897ee355a189
synt4x93 <synt4x.93@gmail.com>
2020-02-06 15:51:05 +0000

    star2lte: update fingerprint override

--- lineage_star2lte.mk
+++ lineage_star2lte.mk
@@ -8,7 +8,7 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_star2lte
 
-BUILD_FINGERPRINT := samsung/star2ltexx/star2lte:9/PPR1.180610.011/G965FXXU4CSE3:user/release-keys
+BUILD_FINGERPRINT := samsung/star2ltexx/star2lte:10/QP1A.190711.020/G965FXXU7DTAA:user/release-keys
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-        PRIVATE_BUILD_DESC="star2ltexx-user 9 PPR1.180610.011 G965FXXU4CSE3 release-keys"
+        PRIVATE_BUILD_DESC="star2ltexx-user 10 QP1A.190711.020 G965FXXU7DTAA release-keys"
