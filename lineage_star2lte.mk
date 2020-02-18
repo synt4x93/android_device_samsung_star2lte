c806d46dcd79f63972781127ca0027fb99654e41
synt4x93 <synt4x.93@gmail.com>
2020-02-18 04:14:56 +0000

    star2lte: update repo

--- lineage_star2lte.mk
+++ lineage_star2lte.mk
@@ -1,12 +1,26 @@
 # Copyright (C) 2018 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 
-$(call inherit-product, device/samsung/star2lte/full_star2lte.mk)
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+$(call inherit-product, vendor/samsung/universal9810-common/star2lte/universal9810-common-vendor.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/star2lte/device.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := star2lte
 PRODUCT_NAME := lineage_star2lte
+PRODUCT_MODEL := SM-G965F
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
 
 BUILD_FINGERPRINT := samsung/star2ltexx/star2lte:10/QP1A.190711.020/G965FXXU7DTAA:user/release-keys
 
