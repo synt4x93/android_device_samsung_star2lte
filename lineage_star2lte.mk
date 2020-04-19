ca4e4a76d4703b0a9f691076bd7c423a10530c1b
synt4x93 <synt4x.93@gmail.com>
2020-04-19 21:40:59 +0100

    star2lte: OTA
    Change-Id: I17c88190c6a70767b9cc993a6e4d0a611776dc49

--- lineage_star2lte.mk
+++ lineage_star2lte.mk
@@ -40,3 +40,7 @@ BUILD_FINGERPRINT := samsung/star2ltexx/star2lte:10/QP1A.190711.020/G965FXXU8DTC
 PRODUCT_BUILD_PROP_OVERRIDES += \
         PRODUCT_NAME=star2ltexx \
         PRIVATE_BUILD_DESC="star2ltexx-user 10 QP1A.190711.020 G965FXXU8DTC5 release-keys"
+
+# OTA
+PRODUCT_PROPERTY_OVERRIDES += \
+    lineage.updater.uri=https://raw.githubusercontent.com/synt4x93/OTA/master/star2lte.json
